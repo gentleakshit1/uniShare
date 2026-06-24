eb47d7610e45e9b0f150b3085cb41bba45e2a755
Akshit Sharma <gentleakshit@gmail.com>
2026-06-24 22:39:35 +0530

    Fix DB env reader

--- unishare/settings.py
+++ unishare/settings.py
@@ -142,8 +142,8 @@ WSGI_APPLICATION = 'unishare.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+    'default': dj_database_url.parse(
+        config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
         conn_max_age=0,
         conn_health_checks=True,
         ssl_require=not DEBUG,
